f83fb1f2168387ed3c40d210c4a44abba6e247fe
lombard001 <sehan.beydullayev12@gmail.com>
2025-08-30 19:37:03 +0400

    Update msp2troll.user.js

--- msp2troll.user.js
+++ msp2troll.user.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://moviestarplanet2.com/*
 // @grant       none
-// @version     1.0
+// @version     3.6.4
 // @author      -
 // @description 03.07.2025 14:14:18
 // ==/UserScript==
